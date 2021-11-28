4915a777685100cb50ad3f9ffae35b61f28c6ab6
Robin Esbjörnsson <robin.esbjornsson@hotmail.com>
2021-11-28 23:52:38 +0100

    fist commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ["mb.cision.com", "links.papareact.com"]
+    },
+    env: {
+        mapbox_key: "pk.eyJ1Ijoicm9iaW5lc2Jqb3Juc3NvbiIsImEiOiJja3dqc2RtNXExNWxmMndtZHYybmpxeG14In0.A68syc77O2ysEOY3_SUf5Q"
+    }
+}
